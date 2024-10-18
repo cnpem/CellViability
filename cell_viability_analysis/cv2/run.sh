ad2f3741072825158fb0cab2bb1dfed267717db9
Daniel-debug-creator <daniel02020202@hotmail.com>
2024-10-18 10:21:13 -0300

    Add template to cell_viability_analysis

---
+++ cell_viability_analysis/cv2/run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#SBATCH --job-name=cv2
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --partition=short-gpu-small
+#SBATCH --gres=gpu:1g.5gb:3
+#SBATCH --mem-per-cpu=4G
+
+time singularity run --nv /opt/images/cellprofiler/cellprofiler-4_2_6.sif -p cv2.cpproj -c -r 
