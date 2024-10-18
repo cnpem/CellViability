7e7bc41ff251e5411c92f767264cc3c10998b82f
Daniel-debug-creator <56095799+Daniel-debug-creator@users.noreply.github.com>
2024-10-18 12:14:37 -0300

    Update run.sh

--- cell_viability_analysis/cv1/run.sh
+++ cell_viability_analysis/cv1/run.sh
@@ -6,4 +6,4 @@
 #SBATCH --gres=gpu:1g.5gb:3
 #SBATCH --mem-per-cpu=4G
 
-time singularity run --nv /opt/images/cellprofiler/cellprofiler-4_2_6.sif -p cv1.cpproj -c -r 
+time singularity run --nv /opt/images/cellprofiler/cellprofiler-4_2_6.sif -p cv1.cppie -c -r 
